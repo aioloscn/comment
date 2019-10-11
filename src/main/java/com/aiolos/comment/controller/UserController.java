e97a445599f2f506b4b8c529b3feecf59d23ec18
aiolos <15988346908@163.com>
2019-10-11 23:48:50 +0800

    initialize

---
+++ src/main/java/com/aiolos/comment/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.aiolos.comment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Aiolos
+ * @date 2019-10-11 23:10
+ */
+@Controller("/user")
+@RequestMapping("/user")
+public class UserController {
+}
